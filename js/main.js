d5245675fa8629bfac979ad3ec6feb20661e192d
Alejandro Meza Zamora <alemz-li@outlook.com>
2022-10-10 16:02:43 -0700

    Added Intersection Observer animation for component list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const themeToggler = document.querySelector('.nav__theme-toggler')
+const setupComponents = document.querySelector('.components')
 
+// Animation
+const showComponents = (entries) => {
+  if (entries[0].isIntersecting) {
+    const componentList = entries[0].target.querySelectorAll('.component__list')
+    componentList.forEach((list) => list.classList.add('visible'))
+  }
+}
+
+const observer = new IntersectionObserver(showComponents, {
+  root: null,
+  rootMargin: '-50px 0px',
+  threshold: 0.1,
+})
+
+observer.observe(setupComponents)
+
+// Theme
 themeToggler.addEventListener('click', () => {
   document.body.classList.toggle('light-theme-variables')
 
